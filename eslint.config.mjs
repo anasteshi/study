791e3bd1310c9cfab2a102b6231d6eb63e5da2ab
anasteshi <anasteshi13@gmail.com>
2025-07-04 12:27:33 +0300

    Format code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,21 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-
+import js from "@eslint/js"
+import globals from "globals"
+import {defineConfig} from "eslint/config"
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-]);
+	{
+		files: ["**/*.{js,mjs,cjs}"],
+		plugins: {js},
+		extends: ["js/recommended"],
+		rules: {
+			"arrow-body-style": ["error", "as-needed"],
+			"no-unused-vars": "off",
+			quotes: ["warn", "double"],
+			indent: ["error", "tab"],
+		},
+	},
+	{
+		files: ["**/*.{js,mjs,cjs}"],
+		languageOptions: {globals: globals.browser},
+	},
+])
