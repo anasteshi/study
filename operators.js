fac11e404f6c6240c3f053abef3969961747af9c
Anastasiia <anastasiia.kholodiuk@gmail.com>
2025-07-06 13:19:21 +0300

    Operators practice

--- operators.js
+++ operators.js
@@ -8,3 +8,10 @@ const hasFreeTime = true
 if (!hasMoney || !hasJob || hasMotivation && hasFreeTime) {
     console.log("Let's code!")
 }
+
+console.log(NaN && 2 && undefined) //NaN
+console.log(1 && 2 && 3) //3
+console.log(!1 && 2 || 3) //3
+console.log(25 || null && !3) //25
+console.log(NaN || null || !3 || undefined || 5) //5
+console.log(NaN || null && !3 && undefined || 5) //5
