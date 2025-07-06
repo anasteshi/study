1fe680d2da7591ff16976dcb4f11d74bdfb4ea47
Anastasiia <anastasiia.kholodiuk@gmail.com>
2025-07-06 13:20:18 +0300

    Clean up code

--- operators.js
+++ operators.js
@@ -5,13 +5,13 @@ const hasJob = false
 const hasMotivation = true
 const hasFreeTime = true
 
-if (!hasMoney || !hasJob || hasMotivation && hasFreeTime) {
-    console.log("Let's code!")
+if (!hasMoney || !hasJob || (hasMotivation && hasFreeTime)) {
+	console.log("Let's code!")
 }
 
 console.log(NaN && 2 && undefined) //NaN
 console.log(1 && 2 && 3) //3
-console.log(!1 && 2 || 3) //3
-console.log(25 || null && !3) //25
+console.log((!1 && 2) || 3) //3
+console.log(25 || (null && !3)) //25
 console.log(NaN || null || !3 || undefined || 5) //5
-console.log(NaN || null && !3 && undefined || 5) //5
+console.log(NaN || (null && !3 && undefined) || 5) //5
