10c6642961891ea7dd9c6dc87cc39aead92c6d1b
Anastasiia <anastasiia.kholodiuk@gmail.com>
2025-07-06 16:23:14 +0300

    Format obj-destructuring.js

--- obj-destructuring.js
+++ obj-destructuring.js
@@ -1,8 +1,8 @@
 const object = {
-    name: "pineapple",
-    expiryDate: "12.12.2025",
-    color: "yellow",
-    price: 23
+	name: "pineapple",
+	expiryDate: "12.12.2025",
+	color: "yellow",
+	price: 23,
 }
 
 const {name, color} = object
@@ -10,23 +10,23 @@ console.log("Color:", color)
 console.log("Name:", name)
 
 const logAddress = ({city, street, houseNumber, apartmentNumber}) => {
-    // const {city, street, houseNumber, apartmentNumber} = address
-    // console.log(`Address: ${city}, ${street}, house ${houseNumber}, apartment ${apartmentNumber}`)
+	// const {city, street, houseNumber, apartmentNumber} = address
+	// console.log(`Address: ${city}, ${street}, house ${houseNumber}, apartment ${apartmentNumber}`)
 }
 
 logAddress({
-    city: "New York",
-    street: "North Street",
-    houseNumber: 24,
-    apartmentNumber: 15
+	city: "New York",
+	street: "North Street",
+	houseNumber: 24,
+	apartmentNumber: 15,
 })
 
 const girl = {
-    name: "Kate",
-    age: 17
+	name: "Kate",
+	age: 17,
 }
 const user = {
-    name: "katexxlyn"
+	name: "katexxlyn",
 }
 const {name: firstName} = girl
 const {name: nickname} = user
@@ -35,15 +35,15 @@ const {age: onlineAge = "Unknown"} = user // Unknown
 
 // Rest parameters
 const logUser = (user) => {
-    const {name, age, city, ...other} = user
-    console.log(`Dear ${name}, you are ${age} years old and you live in ${city}`)
-    console.log(`Besides, I know that you...${JSON.stringify(other)}`)
+	const {name, age, city, ...other} = user
+	console.log(`Dear ${name}, you are ${age} years old and you live in ${city}`)
+	console.log(`Besides, I know that you...${JSON.stringify(other)}`)
 }
 
 logUser({
-    name: "Stanislav",
-    age: 18,
-    city: "Athens",
-    hasJob: true,
-    isHappy: false
+	name: "Stanislav",
+	age: 18,
+	city: "Athens",
+	hasJob: true,
+	isHappy: false,
 })
