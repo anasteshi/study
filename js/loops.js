6407f930d632a500dfadf53203bd963c84f4f86c
Anastasiia <anasteshi13@gmail.com>
2025-07-07 12:26:32 +0300

    Enhance solution

--- js/loops.js
+++ js/loops.js
@@ -2,12 +2,18 @@ const n = 6
 
 let result = ""
 
-for (let i = 1; i <= n; i++) {
-	for (let j = 0; j < i; j++) {
-		result += "*"
-	}
+// for (let i = 1; i <= n; i++) {
+// 	for (let j = 0; j < i; j++) {
+// 		result += "*"
+// 	}
 
-	result += "\n"
+// 	result += "\n"
+// }
+
+// console.log(result)
+
+for (let i = 0; i < n; i++) {
+    result += "*".repeat(i) + "\n"
 }
 
 console.log(result)
