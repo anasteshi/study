77a837ffd055203e617bc8db81c28d19d0b5b165
Anastasiia <anasteshi13@gmail.com>
2025-07-10 18:51:41 +0300

    Add elements to an array – push and unshift methods

---
+++ js/arrays.js
@@ -0,0 +1,7 @@
+const letters = ["A", "B", "C", "D"]
+
+letters.push("E", "F") // Adds elements to the end of the array
+console.log(letters) 
+
+letters.unshift("E", "F") // Adds elements to the start of the array
+console.log(letters) 
