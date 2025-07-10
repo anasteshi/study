b00d07126d200c0b9646245c61064f2dafd978b1
Anastasiia <anasteshi13@gmail.com>
2025-07-10 19:02:36 +0300

    Convert to string using toString and join methods

--- js/arrays.js
+++ js/arrays.js
@@ -1,4 +1,5 @@
 const letters = ["A", "B", "C", "D"]
+const array = ["Apple", true, 1.5]
 
 letters.push("E", "F") // Adds elements to the end of the array
 console.log(letters) 
@@ -9,3 +10,7 @@ console.log(letters)
 console.log(letters.pop()) // Removes the last element of the array and returns this element's value
 
 console.log(letters.shift()) // Removes the first element of the array and returns it
+
+console.log(array.toString()) // Converts types to string, separating them by comas
+
+console.log(array.join(", ")) // Does the same thing as toString(), but the separator can be chosen
