24fac1c12d42e02b198d3d5ac46258896831e95e
Anastasiia <anasteshi13@gmail.com>
2025-07-10 19:18:18 +0300

    Copy arrays using spread operator and slice method

--- js/arrays.js
+++ js/arrays.js
@@ -1,5 +1,8 @@
 const letters = ["A", "B", "C", "D"]
-const array = ["Apple", true, 1.5]
+const array1 = ["Apple", true, 1.5]
+const array2 = []
+const array3 = [...array1] 
+const array4 = array1.slice() // Returns a copy of a section of the array
 const message = "One, Two, Three, Four, Five, Six"
 
 letters.push("E", "F") // Adds elements to the end of the array
@@ -12,8 +15,18 @@ console.log(letters.pop()) // Removes the last element of the array and returns 
 
 console.log(letters.shift()) // Removes the first element of the array and returns it
 
-console.log(array.toString()) // Converts types to string, separating them by comas
+console.log(array1.toString()) // Converts types to string, separating them by comas
 
-console.log(array.join(", ")) // Does the same thing as toString(), but the separator can be chosen
+console.log(array1.join(", ")) // Does the same thing as toString(), but the separator can be chosen
 
 console.log(message.split(", ").join(", "))
+
+for (let i = 0; i < array1.length; i++) {
+    array2.push(array1[i])
+}
+
+array2[0] = "Peach"
+
+console.log(array1)
+console.log(array2)
+console.log(array3)
