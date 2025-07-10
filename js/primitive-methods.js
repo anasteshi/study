98593155d320351f27dfa159e4e140406c3af05b
Anastasiia <anasteshi13@gmail.com>
2025-07-10 11:17:51 +0300

    ToString method

--- js/primitive-methods.js
+++ js/primitive-methods.js
@@ -1,4 +1,6 @@
 const price = 99.99
+const number = 100
+const numberString = number.toString(2) // 1100100 – toString() has an optional parameter for the base of a system of numeration (radix).
 
 console.log("Round to 0 digits: ", price.toFixed())
 console.log("Round to 2 digits: ", price.toFixed(2))
@@ -9,3 +11,5 @@ console.log((4.555).toFixed(2))
 
 console.log(price.toPrecision(4))
 console.log(price.toPrecision(3))
+
+console.log(numberString)
