06ac0226e255c514ebd1befe54ce4fb663937f6a
Anastasiia <anasteshi13@gmail.com>
2025-07-10 11:10:57 +0300

    ToFixed method

---
+++ js/primitive-methods.js
@@ -0,0 +1,8 @@
+const price = 99.99
+
+console.log("Round to 0 digits: ", price.toFixed())
+console.log("Round to 2 digits: ", price.toFixed(2))
+console.log("Round to 4 digits: ", price.toFixed(4))
+
+console.log(4..toFixed(3))
+console.log((4.555).toFixed(2))
