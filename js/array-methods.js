0750257c14e3ded72629f4d8b38a00e4e8659ee5
Anastasiia <anasteshi13@gmail.com>
2025-07-11 11:29:55 +0300

    Map method

--- js/array-methods.js
+++ js/array-methods.js
@@ -4,18 +4,19 @@ const letters = ["A", "B", "C", "D"]
 const prices = [11, 22, 33, 44, 55, 44]
 const people = [
 	{
-		status: "Married",
+		status: "married",
 		age: 25,
 	},
 	{
-		status: "Married",
+		status: "married",
 		age: 34,
 	},
 	{
-		status: "Single",
+		status: "single",
 		age: 18,
 	},
 ]
+const formattedPeople = people.map((person) => `This person is ${person.age} years old and is ${person.status}`) // Calls a defined callback function on each element of an array, and returns an array that contains the results.
 
 console.log(name)
 console.log(age)
@@ -51,18 +52,26 @@ console.log(prices.indexOf(44, 4)) // 5 – indexOf has a second optional parame
 
 console.log(prices.lastIndexOf(44, 4)) // 3 – lastIndexOf does the same thing, but going to the left.
 
-console.log(people.indexOf({status: "Married", age: 25})) // DOESN'T work with objects because of their reference-type behavior
+console.log(people.indexOf({status: "married", age: 25})) // DOESN'T work with objects because of their reference-type behavior
 
 console.log(people.findIndex((person) => person.age === 18)) // Returns the index of the first element in the array where predicate is true
 console.log(people.findLastIndex((person) => person.age === 25)) // Same but from right to left
 
 console.log(prices.includes(22)) // true – also has an optional parameter for starting index
-console.log(people.includes({status: "Married", age: 34})) // DOESN'T work with objects because of their reference-type behavior
+console.log(people.includes({status: "married", age: 34})) // DOESN'T work with objects because of their reference-type behavior
 
 console.log(people.some((person) => person.age === 34)) // Determines whether the specified callback function returns true for any element of an array.
 
 console.log(people.every((person) => person.age === 10)) // Determines whether all the members of an array satisfy the specified test.
 
-console.log(people.find((person) => person.status === "Single")) // Returns the value of the first element in the array where predicate is true, and undefined otherwise.
+console.log(people.find((person) => person.status === "single")) // Returns the value of the first element in the array where predicate is true, and undefined otherwise.
 
-console.log(people.filter((person) => person.status === "Married")) // Returns the elements of an array that meet the condition specified in a callback function.
+console.log(people.filter((person) => person.status === "married")) // Returns the elements of an array that meet the condition specified in a callback function.
+
+console.log(formattedPeople) /*
+[
+    'This person is 25 years old and is married',
+    'This person is 34 years old and is married',
+    'This person is 18 years old and is single'
+]
+*/
