b5c564dc62514f0b2b39e0ea287f660b6a130fc2
Anastasiia <anasteshi13@gmail.com>
2025-07-11 10:18:08 +0300

    ForEach method

--- js/array-methods.js
+++ js/array-methods.js
@@ -1,5 +1,15 @@
 const array = ["Anna", 3]
 const [name, age] = array
+const letters = ["A", "B", "C", "D"]
 
 console.log(name)
 console.log(age)
+
+letters.forEach((element, index, array) => { // Does the same thing as for loop, but way better
+    console.log(`For the index ${index} we have a letter ${element}`) // All the parameters are optional
+    console.log(`Here's the array we're working with ${array}`)
+})
+
+letters.forEach((letter) => {
+    console.log(letter.concat("AAA"))
+})
